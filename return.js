674ab9aafe06a12122e22a9af5b84c9464ef1a2a
Daniel Taherkhanian <daniel.taherkhanian@elev.ga.ntig.se>
2023-04-27 13:33:04 +0200

    V 1.0 Finished Version

--- return.js
+++ return.js
@@ -1,9 +1,9 @@
 
-//let ReturnButton = document.querySelector(".return-button");
+let ReturnButton = document.querySelector(".return-button");
 
-//ReturnButton.addEventListener("click", back);
+ReturnButton.addEventListener("click", back);
 
 
-//function back(){
-//    window.location.replace("./index.html");
-//}
+function back(){
+    window.location.replace("./index.html");
+}
